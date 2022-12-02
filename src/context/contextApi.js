b14d4ac994b82415909e32c534dcfeff9f2f90cf
devalencia12 <devalencia@bancodeoccidente.com.co>
2022-12-02 14:55:03 -0500

    :sparkles: LeftNav and Header building and consuming Youtube API

---
+++ src/context/contextApi.js
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResult, setSearchResult] = useState(false);
+  const [selectCategory, setSelectCategory] = useState("New");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectedCategoryData(selectCategory);
+  }, [selectCategory]);
+
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchDataFromApi(`search/?q=${query}`).then(({contents}) => {
+      console.log(contents);
+      setSearchResult(contents)
+      setLoading(false);
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        searchResult,
+        setSearchResult,
+        selectCategory,
+        setSelectCategory,
+        mobileMenu,
+        setMobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
